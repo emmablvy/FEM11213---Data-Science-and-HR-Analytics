2e139b9d394ff95f47ed8568f898cd6b2852bd33
emmablvy <149567541+emmablvy@users.noreply.github.com>
2023-11-06 20:22:35 +0100

    Update README.R

--- README.R
+++ README.R
@@ -1,4 +1,4 @@
-# FEM11213---Data-Science-and-HR-Analytics
+a# FEM11213---Data-Science-and-HR-Analytics
 Student participating in the course FEM11213 - Data Science and HR Analytics
 
 Input from Professor Kapoor: 
@@ -7,8 +7,8 @@ Consider the bootstrap loop we discussed:
   B <- 1000
   mub <- c()
   for (b in 1:1000){
-    samp_b <- sample.int(nrow(browser), replace=TRUE)
-    mub <- c(mub, mean(browser$spend[samp_b]))
+    samp_b <- sample.int(nrow(browser_totalspend), replace=TRUE)
+    mub <- c(mub, mean(browser_totalspend$spend[samp_b]))
   }
   sd(mub)
 
@@ -21,8 +21,39 @@ B = 10 --> sd [1] 78.81825
 Hence, the lower B is set, the lower the sd received ceteris paribus. Hence, increasing the number of bootstrap samples (here B), the estimates derived become more precise and hence the standard deviation becomes smaller with a larger number of samples.
 Also according to CLT, increasing the number of bootstraps, the distribution should converge towards the true distribution. Therefore, the number should at least be higher than the threshold of N = 30 (?).
 
+Create a plot with following code:
 
- 
+# Initialize an empty vector to store standard deviations
+sd_values <- c()
+
+# Specify a range of values for B
+B_values <- c(10, 50, 100, 500, 1000, 5000)
+
+# Loop over different values of B
+for (B in B_values) {
+  mub <- c()
+  
+  # Perform the bootstrap procedure
+  for (b in 1:B){
+    samp_b <- sample.int(nrow(browser_totalspend), replace=TRUE)
+    mub <- c(mub, mean(browser_totalspend$spend[samp_b]))
+  }
+  
+  # Calculate and store the standard deviation
+  sd_values <- c(sd_values, sd(mub))
+}
+
+# Plot the results
+plot(B_values, sd_values, type = 'b', 
+     xlab = 'Number of Bootstrap Samples (B)', 
+     ylab = 'Standard Deviation of Means',
+     main = 'Impact of B on Bootstrap Standard Deviation')
+
+Output:
+
+
+
+
 3. Interpret
 
 
